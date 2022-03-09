768937776966d3264354361bcf93b54076c064c9
pblgllgs <pbl.gllgs@gmail.com>
2022-03-09 08:50:09 -0300

    refactor custom middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,7 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
 const newUser = (req, res = response) => {
     const { name, email, password } = req.body;
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped(),
-        });
-    }
     res.status(201).json({
         ok: true,
         msg: 'registro',
@@ -21,13 +13,6 @@ const newUser = (req, res = response) => {
 
 const login = (req, res = response) => {
     const { email, password } = req.body;
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped(),
-        });
-    }
     res.status(200).json({
         ok: true,
         msg: 'registro',
