9a9fcfb0937a16632a78cfe47bb90539e8975c3b
pblgllgs <pbl.gllgs@gmail.com>
2022-03-10 09:57:18 -0300

    create validarJWT

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,28 @@
+const { request } = require('express');
+const { response } = require('express');
+const jwt = require('jsonwebtoken');
+
+const validarJWT = (req = request, res = response, next) => {
+    const token = req.header('x-token');
+    if (!token) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'Token de authenticación no encontrado',
+        });
+    }
+    try {
+        const {uid,name} = jwt.verify(token, process.env.SECRET_JWT_SEED);
+
+        req.uid = uid;
+        req.name = name;
+    } catch (error) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'Token no válido',
+        });
+    }
+
+    next();
+};
+
+module.exports = { validarJWT };
