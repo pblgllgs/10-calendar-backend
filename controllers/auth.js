a87fa2d3d80ec84d43f2ad01eb374f664979a15c
pblgllgs <pbl.gllgs@gmail.com>
2022-03-10 09:32:16 -0300

    update login with jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const { response } = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { generarJWT } = require('../helpers/jwt');
 
 const newUser = async (req, res = response) => {
     const { email, password } = req.body;
@@ -16,31 +18,57 @@ const newUser = async (req, res = response) => {
         const salt = bcrypt.genSaltSync(10);
         user.password = bcrypt.hashSync(password, salt);
         await user.save();
+        const token = await generarJWT(user.uid, user.name);
         res.status(201).json({
             ok: true,
             uid: user.id,
-            name: user.name
+            name: user.name,
+            token,
         });
     } catch (error) {
         console.log(error);
         res.status(500).json({
             ok: false,
-            msg: 'Error al guardar'
+            msg: 'Error al guardar',
         });
     }
 };
 
-const login = (req, res = response) => {
+const login = async (req, res = response) => {
     const { email, password } = req.body;
-    res.status(200).json({
-        ok: true,
-        msg: 'registro',
-        email,
-        password,
-    });
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Credenciales incorrectas',
+            });
+        }
+        const validPassword = bcrypt.compareSync(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Credensiales incorrectas',
+            });
+        }
+        const token = await generarJWT(user.uid, user.name);
+        res.status(200).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+            token,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error al inicial sesión',
+        });
+    }
 };
 
 const renewToken = (req, res = response) => {
+    
     res.json({
         ok: true,
         msg: 'renew',
