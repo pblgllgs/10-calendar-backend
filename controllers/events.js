7d20ff1199eaffc0f789f9ea54e393a8354d44f7
pblgllgs <pbl.gllgs@gmail.com>
2022-03-11 11:20:30 -0300

    update eliminar y actualizar

--- controllers/events.js
+++ controllers/events.js
@@ -1,10 +1,13 @@
 const { response } = require('express');
+const Evento = require('../models/Evento');
 
 const obtenerEventos = async (req, res = response) => {
     try {
+        const eventos = await Evento.find().populate('user', 'name');
         res.status(200).json({
             ok: true,
             msg: 'Obteniedo eventos',
+            eventos,
         });
     } catch (error) {
         console.log(error);
@@ -17,9 +20,13 @@ const obtenerEventos = async (req, res = response) => {
 
 const crearEvento = async (req, res = response) => {
     try {
+        const nuevoEvento = new Evento(req.body);
+        nuevoEvento.user = req.uid;
+        const eventoDB = await nuevoEvento.save();
         res.status(200).json({
             ok: true,
-            msg: 'Creando evento',
+            msg: 'Evento creado evento',
+            eventoDB,
         });
     } catch (error) {
         console.log(error);
@@ -31,36 +38,72 @@ const crearEvento = async (req, res = response) => {
 };
 
 const actualizarEvento = async (req, res = response) => {
-
-    const id = req.params;
+    const eventoId = req.params.id;
+    const uid = req.uid;
     try {
+        const evento = await Evento.findById(eventoId);
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe el evento que desa modificar',
+            });
+        }
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene autorización para modificar este evento',
+            });
+        }
+        const eventoNuevo = {
+            ...req.body,
+            user: uid,
+        };
+        const eventoActualizado = await Evento.findByIdAndUpdate(
+            eventoId,
+            eventoNuevo,
+            { new: true }
+        );
         res.status(200).json({
             ok: true,
-            msg: 'Actualizar  evento',
-            id
+            evento: eventoActualizado,
         });
     } catch (error) {
         console.log(error);
         res.status(500).json({
             ok: false,
-            msg: 'Error al actualizar eventos',
+            msg: 'Error al actualizar evento',
         });
     }
 };
 
 const eliminarEvento = async (req, res = response) => {
-    const id = req.params;
+    const eventoId = req.params.id;
+    const uid = req.uid;
     try {
+        const evento = await Evento.findById(eventoId);
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe el evento que desa eliminar',
+            });
+        }
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene autorización para eliminar este evento',
+            });
+        }
+        const eventoEliminado = await Evento.findByIdAndDelete(eventoId);
         res.status(200).json({
             ok: true,
-            msg: 'Eliminar  evento',
-            id
+            msg: 'Evento eliminado',
+            evento: eventoEliminado,
         });
     } catch (error) {
         console.log(error);
         res.status(500).json({
             ok: false,
-            msg: 'Error al actualizar eventos',
+            msg: 'Error al eliminar evento',
         });
     }
 };
