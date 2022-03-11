73e8d911925605d7dcd5fc16af82ffdc67a749f1
pblgllgs <pbl.gllgs@gmail.com>
2022-03-11 11:19:43 -0300

    create model evento

---
+++ models/Evento.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const EventoSchema = Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    notes: {
+        type: String,
+    },
+    start: {
+        type: Date,
+        required: true,
+    },
+    end: {
+        type: Date,
+        required: true,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+});
+
+//se oculta la version del doc y se cambia _id por id solo para efecto de procesado datos
+EventoSchema.method('toJSON', function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+});
+
+module.exports = model('Evento', EventoSchema);
